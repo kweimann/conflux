a29c34dffee5a83e454a4445c43516316627f4b8
Kuba Weimann <kuba.weimann@gmail.com>
2018-03-17 15:17:52 +0100

    adds setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name="conflux",
+      version=0.1,
+      description="time series utility library for Python",
+      url="https://github.com/kweimann/conflux",
+      author="Kuba Weimann",
+      author_email="kuba.weimann@gmail.com",
+      license="MIT",
+      packages=["conflux"],
+      install_requires=["numpy"],
+      zip_safe=False)
